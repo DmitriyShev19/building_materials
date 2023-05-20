5a6646ac457fed7a9eb2058f3b3028cdfb2db18a
dimka2338 <dimka2338@ya.ru>
2023-05-20 20:08:43 +0300

    Добавил дефолтную картинку, создал модель, добавил в шаблоны include

--- product_catalog/models.py
+++ product_catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    short_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='construction_products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.short_name
+
