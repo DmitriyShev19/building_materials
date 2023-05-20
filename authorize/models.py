ef938ca3d494265dce9f84be3ad356098348830e
dimka2338 <dimka2338@ya.ru>
2023-05-20 16:52:44 +0300

    Сделал поля не обязательными

--- authorize/models.py
+++ authorize/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class Person(AbstractUser):
-    city = models.CharField(max_length=100)
-    street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=20)
-    phone_numbers = models.CharField(max_length=20)
-    avatar = models.ImageField(upload_to='photos/%Y/%m/%d')
+    city = models.CharField(max_length=100, null=True)
+    street = models.CharField(max_length=100, null=True)
+    house_number = models.CharField(max_length=20, null=True)
+    phone_numbers = models.CharField(max_length=20, null=True)
+    avatar = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
 
 
