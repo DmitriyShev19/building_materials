33552684e634f74c3f9ada937d68b4ba89da2527
dimka2338 <dimka2338@ya.ru>
2023-05-20 23:30:20 +0300

    добавил что поля индексируемые

--- product_catalog/models.py
+++ product_catalog/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     short_name = models.CharField(max_length=50)
     full_name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='construction_products/')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.short_name
+
 
