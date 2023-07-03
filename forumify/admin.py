2899d507d3dea9911ac41acbaebcf9db65924349
dimka2338 <dimka2338@ya.ru>
2023-07-03 21:37:32 +0300

    Добавил в админку модели Post и Comment

--- forumify/admin.py
+++ forumify/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Topic
+from .models import Category, Topic, Post, Comment
 
 admin.site.register(Category)
 admin.site.register(Topic)
+admin.site.register(Post)
+admin.site.register(Comment)
