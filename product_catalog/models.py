d57fa7e60fabd189187e9b9ea1959277de8bfb7c
dimka2338 <dimka2338@ya.ru>
2023-07-18 19:40:21 +0300

    Добавил метод get_price который считает общую стоимость товаров этой категории в корзине

--- product_catalog/models.py
+++ product_catalog/models.py
@@ -67,6 +67,9 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
+    def get_price(self):
+        return self.product.price * self.quantity
+
     def __str__(self):
         return f"{self.product} (Quantity: {self.quantity})"
 
