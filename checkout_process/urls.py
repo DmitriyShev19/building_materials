1d8c4bcf99da668f25620c30cc3fd6714487efb8
dimka2338 <dimka2338@ya.ru>
2023-07-18 19:36:55 +0300

    Создал шаблон

--- checkout_process/urls.py
+++ checkout_process/urls.py
@@ -3,7 +3,5 @@ from django.views.decorators.cache import cache_page
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('profile/', profile, name='profile'),
-    path('edit_person/', edit_person, name='edit_person'),
+    path('', CheckView.as_view(), name='checkout'),
 ]
