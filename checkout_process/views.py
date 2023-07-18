2f513f8182e4a6ab7cf5dfeeea50334b67d8293c
dimka2338 <dimka2338@ya.ru>
2023-07-18 19:35:50 +0300

    Сделал проверку заполнения полей, если они пустые то перекидывает на форму заполнения

--- checkout_process/views.py
+++ checkout_process/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+
+class CheckView(LoginRequiredMixin, View):
+    login_url = 'account_login'
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if not (user.city and user.street and user.house_number and user.phone_numbers) :
+            return redirect(reverse('edit_person'))
+        return HttpResponse("Success!")
+
