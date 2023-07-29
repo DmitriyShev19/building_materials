423075419798b589bd9ad04683d12ebdce7e4474
dimka2338 <dimka2338@ya.ru>
2023-07-29 17:02:50 +0300

    Изменил перенаправление страницы на страницу оплаты

--- checkout_process/views.py
+++ checkout_process/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.views import View
 
@@ -12,5 +12,5 @@ class CheckView(LoginRequiredMixin, View):
         user = request.user
         if not (user.city and user.street and user.house_number and user.phone_numbers) :
             return redirect(reverse('edit_person'))
-        return HttpResponse("Success!")
+        return redirect(reverse('payment_page'))
 
