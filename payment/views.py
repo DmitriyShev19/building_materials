17e7580f249d43a9c1ef493b9c10490170b44228
dimka2338 <dimka2338@ya.ru>
2023-07-29 20:23:29 +0300

    Построил логику получения суммы и создания страницы платежа через crystalpayAPI

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.views import View
+import os
 
-# Create your views here.
+from .crystalpay_sdk import CrystalPAY, InvoiceType
+from product_catalog.views import count_price
+
+
+crystalpayAPI = CrystalPAY(os.environ.get('AUTH_LOGIN'),
+                           os.environ.get('SECRET1'),
+                           os.environ.get('SECRET2')
+                           )
+
+
+class PayCartView(LoginRequiredMixin, View):
+    login_url = 'account_login'
+
+    def get(self, request, *args, **kwargs):
+        dict_pay = crystalpayAPI.Invoice.create(count_price(request),
+                                                          InvoiceType.purchase,
+                                                          15)
+        return redirect(dict_pay['url'])
